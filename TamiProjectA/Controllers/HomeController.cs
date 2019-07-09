be6ac113a1debdab5d83591c02fb8326f02892e8
Tami R <tami1989@yahoo.com>
2019-07-08 21:03:00 -0500

    second

--- TamiProjectA/Controllers/HomeController.cs
+++ TamiProjectA/Controllers/HomeController.cs
@@ -4,15 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TamiProjectA.Data;
 using TamiProjectA.Models;
 
 namespace TamiProjectA.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+
         public IActionResult Index()
         {
-            return View();
+            return View(_db.line_items.ToList());
+
+            //return View();
         }
 
         public IActionResult About()
